46a50eddf41c5a6f7808b88f4e7f19cc1229588f
Kanisorn S <firstziiz.k@gmail.com>
2018-09-12 22:46:06 +0700

    Initial ProfilePage

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import requireAuth from '../utils/requireAuth'
+import { inject, observer } from 'mobx-react'
 
 @requireAuth()
+@inject('user')
+@observer
 class ProfilePage extends React.Component {
   render() {
-    return <div>ProfilePage</div>
+    return (
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col">
+            <h1>
+              Hi, {this.props.user.user.first_name} {this.props.user.user.last_name} 🌙
+            </h1>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
