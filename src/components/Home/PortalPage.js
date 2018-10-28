6724f17cc84f4ad6c85e004ca782d204c4a17b6a
Kanisorn S <firstziiz.k@gmail.com>
2018-10-29 03:29:23 +0700

    [ADD] Add Portal Page for logged In User

---
+++ src/components/Home/PortalPage.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import { inject, observer } from 'mobx-react'
+import styled, { keyframes } from 'styled-components'
+import Particles from 'react-particles-js'
+
+import Layout from '../Core/Layout'
+import ParticleConfig from '../../static/particle.config.json'
+import Card from '../Core/Card'
+import TaskItem from '../Task/TaskItem'
+import { Link } from 'react-static'
+
+const FrontLayout = styled.div`
+  height: 100%;
+
+  .particle {
+    height: calc(100% - 56px);
+    position: fixed;
+    z-index: 1;
+    width: 100%;
+  }
+
+  .container {
+    z-index: 2;
+  }
+`
+
+@inject('user')
+@observer
+class ProtalPage extends React.Component {
+  render() {
+    return (
+      <FrontLayout>
+        <Layout>
+          <Particles className="particle" params={ParticleConfig} />
+          <div className="foreground" />
+          <div className="container position-relative pt-4">
+            <div className="row mb-3">
+              <div className="col">
+                <Card>
+                  <div className="text-center px-4">
+                    <h3 className="mb-0">ทำโจทย์ข้อนี้ต่อไหม ?</h3>
+                    <p className="px-4">
+                      โจทย์ข้อนี้ยังทำไม่เสร็จเลย มาแก้โจทย์ปัญหานี้กันต่อดีกว่า
+                    </p>
+                    <TaskItem name="Lorem..." difficult={`BEGINNER`} topic={`String`} />
+                  </div>
+                </Card>
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col">
+                <Card>
+                  <div className="text-center px-4">
+                    <h3>ลองทำโจทย์ข้อนี้ดูสิ</h3>
+                    <TaskItem name="Lorem..." difficult={`INTERMEDIATE`} topic={`String`} />
+                    <TaskItem name="Lorem..." difficult={`BEGINNER`} topic={`String`} />
+                    <TaskItem name="Lorem..." difficult={`ADVANCE`} topic={`String`} />
+                  </div>
+                </Card>
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col">
+                <Card>
+                  <div className="text-center px-4">
+                    <h3>โจทย์มาใหม่ 🎉</h3>
+                    <p>
+                      <Link to="/challange">ดูโจทย์ทั้งหมด</Link>
+                    </p>
+                    <TaskItem
+                      name="Lorem..."
+                      difficult={`BEGINNER`}
+                      topic={`String`}
+                      passCount={12192}
+                    />
+                    <TaskItem name="Lorem..." difficult={`INTERMEDIATE`} topic={`String`} />
+                    <TaskItem name="Lorem..." difficult={`INTERMEDIATE`} topic={`String`} />
+                    <TaskItem name="Lorem..." difficult={`BEGINNER`} topic={`String`} />
+                    <TaskItem name="Lorem..." difficult={`INTERMEDIATE`} topic={`String`} />
+                    <TaskItem name="Lorem..." difficult={`BEGINNER`} topic={`String`} />
+                    <TaskItem name="Lorem..." difficult={`INTERMEDIATE`} topic={`String`} />
+                  </div>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </FrontLayout>
+    )
+  }
+}
+
+export default ProtalPage
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,113 +1,18 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
-
-import Particles from 'react-particles-js'
-import ParticleConfig from '../static/particle.config.json'
-
-import Layout from '../components/Core/Layout'
-import Astronaut from '../static/images/astronaut.png'
-import Moon from '../static/images/moon.png'
 import { inject, observer } from 'mobx-react'
 
-const FloatAstro = keyframes`
-  0% {
-      transform: translateY(0)
-  }
+import LandingPage from '../components/Home/LandingPage'
+import PortalPage from '../components/Home/PortalPage'
 
-  50% {
-      transform: translateY(1rem)
-  }
-
-  100% {
-      transform: translateY(0)
-  }
-`
-
-const FrontLayout = styled.div`
-  height: 100%;
-
-  .foreground {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    background-size: 400px;
-    z-index: 0;
-  }
-
-  h1,
-  h3 {
-    color: white;
-  }
-
-  .container {
-    z-index: 2;
-    height: calc(100% - 56px);
-  }
-
-  .headline {
-    position: absolute;
-    top: 25%;
-
-    h1 {
-      font-size: 7rem;
-      margin: 0;
-    }
-
-    h3 {
-      white-space: pre-line;
-    }
-  }
-
-  .astronaut {
-    position: absolute;
-    height: 20rem;
-    right: 0;
-    bottom: 10%;
-    animation: ${FloatAstro} 6s infinite normal;
-  }
-
-  .moon {
-    position: absolute;
-    height: 15rem;
-    right: 20%;
-    top: 10%;
-    animation: ${FloatAstro} 10s infinite normal 3s;
-  }
-
-  .particle {
-    height: calc(100% - 56px);
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-  }
-`
 @inject('user')
 @observer
 class Home extends React.Component {
   render() {
-    return (
-      <FrontLayout>
-        <Layout>
-          <Particles className="particle" params={ParticleConfig} />
-          <div className="foreground" />
-          <div className="container position-relative">
-            <div className="headline">
-              <h1>Luna</h1>
-              <h3>{`Web Applicaition for\n Improving Programming Skills`}</h3>
-              <button className="btn btn-luna" onClick={() => this.props.user.setLoginModal(true)}>
-                เริ่มต้นออกเดินทาง
-              </button>
-            </div>
-            <img src={Astronaut} alt="Luna Astronaut" className="astronaut" />
-            <img src={Moon} alt="Luna Moon" className="moon" />
-          </div>
-        </Layout>
-      </FrontLayout>
-    )
+    const { user } = this.props
+    if (user.authenticated) {
+      return <PortalPage />
+    }
+    return <LandingPage />
   }
 }
 
