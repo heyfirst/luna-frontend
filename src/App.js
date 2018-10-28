99707dcb5020b8e67a0fb2a83e6a2baa87ed805e
Kanisorn S <firstziiz.k@gmail.com>
2018-10-29 01:11:29 +0700

    [UPDATE] update new background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { injectGlobal } from 'styled-components'
 import { Route as ReactRoute, Switch, withRouter, Redirect } from 'react-static'
 import { hot } from 'react-hot-loader'
 import * as R from 'ramda'
@@ -6,7 +7,6 @@ import { observer, inject } from 'mobx-react'
 
 import './App.css'
 import NotFound from './containers/404'
-import Login from './containers/Login'
 import TopicPage from './containers/TopicPage'
 import Home from './containers/Home'
 import TaskListPage from './containers/TaskListPage'
@@ -15,6 +15,15 @@ import ProfilePage from './containers/ProfilePage'
 
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
+
+import Background from './static/images/bg.png'
+
+injectGlobal`
+  body {
+    background: url(${Background});
+    background-size: cover;
+  }
+`
 
 class Route extends React.Component {
   componentWillMount() {
@@ -67,7 +76,6 @@ class App extends React.Component {
           render={props => <Redirect to={`${props.location.pathname}/`} />}
         />
         <Switch>
-          <Route key={1} exact path="/login" component={Login} />
           <Route key={2} exact path="/" component={Home} />
           <Route key={3} exact path="/topics" component={TopicPage} />
           <Route key={4} exact path="/topics/:topicID" component={TaskListPage} />
