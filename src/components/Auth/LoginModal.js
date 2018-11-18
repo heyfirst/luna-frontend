351ac608201fdb5548ebb4f2ee021ad124e19ec7
Kanisorn S <firstziiz.k@gmail.com>
2018-11-18 14:09:55 +0700

    [UPDATE] cosmetic of login modal

--- src/components/Auth/LoginModal.js
+++ src/components/Auth/LoginModal.js
@@ -1,6 +1,16 @@
 import React from 'react'
-import { Button, Modal } from 'antd'
+import styled from 'styled-components'
+import { Button, Modal, Icon } from 'antd'
 import { inject, observer } from 'mobx-react'
+import LoginShip from '../../static/images/login-ship.png'
+
+const ShipImage = styled.img`
+  transform: rotate(17.5deg);
+`
+
+const LoginButton = styled(Button)`
+  font-size: 1rem;
+`
 
 @inject('user')
 @observer
@@ -35,9 +45,15 @@ class LoginModal extends React.Component {
         onOk={() => this.props.user.setLoginModal(false)}
         onCancel={() => this.props.user.setLoginModal(false)}
       >
-        <Button type="primary" onClick={() => this.onLogin()}>
-          Sign In with Facebook
-        </Button>
+        <div className="d-flex flex-column">
+          <div className="text-center mb-2">
+            <ShipImage src={LoginShip} alt="luna-login-ship" className="img-fluid w-50 p-1" />
+          </div>
+          <h4 className="text-center">เข้าสู่ระบบก่อนออกเดินทาง</h4>
+          <LoginButton type="primary" onClick={() => this.onLogin()}>
+            <Icon type="facebook" theme="filled" /> เข้าสู่ระบบด้วย Facebook
+          </LoginButton>
+        </div>
       </Modal>
     )
   }
