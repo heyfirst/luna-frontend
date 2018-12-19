9263d7f0c46572f2db858461a5ad45b69e6da639
Kanisorn S <firstziiz.k@gmail.com>
2018-12-20 03:40:01 +0700

    [UPDATE] Calendar Chart

--- src/components/Profile/CalendarHeatmapCard.js
+++ src/components/Profile/CalendarHeatmapCard.js
@@ -10,6 +10,12 @@ import store from './store'
 
 let today = new Date()
 
+const getLastday = today => {
+  let day = today.getDay() + 1
+
+  return -7 * 50 - day
+}
+
 function shiftDate(date, numDays) {
   const newDate = new Date(date)
   newDate.setDate(newDate.getDate() + numDays)
@@ -24,18 +30,15 @@ class CalendarHeatmapCard extends React.Component {
   }
 
   getFrequencyData = () => {
-    return this.state.data.map(data => {
-      return {
-        date: data.day,
-        count: data.total
-      }
-    })
+    return this.state.data.map(data => ({
+      date: data.day,
+      count: data.total
+    }))
   }
 
   async componentWillMount() {
     await store.fetchUser(this.props.match.params.id)
     let data = await UserService.getFrequencyPractics(store.user.username).then(resp => resp.data)
-
     this.setState({
       data
     })
@@ -47,22 +50,21 @@ class CalendarHeatmapCard extends React.Component {
         <h5>ความถี่ในการทำโจทย์</h5>
         <hr />
         <CalendarHeatmap
-          startDate={shiftDate(today, -300)}
+          startDate={shiftDate(today, getLastday(today))}
           endDate={today}
           values={this.getFrequencyData()}
           classForValue={value => {
             if (!value) {
               return 'color-empty'
             }
-            return `color-github-${value.count}`
+            return `color-github-${value.count < 4 ? value.count : 4}`
           }}
           tooltipDataAttrs={value => {
             return {
-              'data-tip': `ทำสำเร็จทั้งหมด: ${value.count || '-'} ข้อ`
+              'data-tip': `วันที่ x | ทำสำเร็จทั้งหมด: ${value.count || '0'} ข้อ`
             }
           }}
           showWeekdayLabels
-          onClick={value => alert(`Clicked on value with count: ${value.count}`)}
         />
         <ReactTooltip />
       </Card>
